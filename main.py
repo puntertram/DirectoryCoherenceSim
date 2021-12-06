00b204c13c0233823b678e95d25a18b831e11ea8
puntertram <puneethnaik60@gmail.com>
2021-12-06 10:23:48 +0530

    Incorporate second scenario

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from MemoryController import MemoryController
 class Message:
     def __init__(self, type, source, destination) -> None:
         self.type = type 
@@ -22,12 +22,35 @@ class GetSMessage(Message):
     def __str__(self) -> str:
         return f"<{self.type}Message address={self.address}, source={self.source}, destination={self.destination}>"
 
+class AckMessage(Message):
+    def __init__(self, address, source, destination) -> None:
+        super().__init__(type="Ack", source=source, destination=destination)
+        self.address = address
+    def __str__(self) -> str:
+        return f"<{self.type}Message address={self.address}, source={self.source}, destination={self.destination}>"
+
 class GetMMessage(Message):
     def __init__(self, address, source, destination) -> None:
         super().__init__(type="GetM", source=source, destination=destination)
         self.address = address
     def __str__(self) -> str:
         return f"<{self.type}Message address={self.address}, source={self.source}, destination={self.destination}>"
+
+class DataRespAckMessage(Message):
+    def __init__(self, address, ack, source, destination) -> None:
+        super().__init__(type="DataRespAck", source=source, destination=destination)
+        self.address = address
+        self.ack = ack
+    def __str__(self) -> str:
+        return f"<{self.type}Message address={self.address}, ack = {self.ack} source={self.source}, destination={self.destination}>"
+
+class InvalidateMessage(Message):
+    def __init__(self, address, requester, source, destination) -> None:
+        super().__init__(type="Invalidate", source=source, destination=destination)
+        self.address = address
+        self.requester = requester
+    def __str__(self) -> str:
+        return f"<{self.type}Message address={self.address}, requester = {self.requester} source={self.source}, destination={self.destination}>"
 
 
 class FwdGetSMessage(Message):
@@ -45,6 +68,23 @@ class DataRespMessage(Message):
         self.data = data      
     def __str__(self) -> str:
         return f"<{self.type}Message address={self.address}, data={self.data}, source={self.source}, destination={self.destination}>"
+
+class DataRespLoadMessage(Message):
+    def __init__(self, address, data, source, destination) -> None:
+        super().__init__(type="DataRespLoad", source=source, destination=destination)
+        self.address = address
+        self.data = data      
+    def __str__(self) -> str:
+        return f"<{self.type}Message address={self.address}, data={self.data}, source={self.source}, destination={self.destination}>"
+
+class DataRespStoreMessage(Message):
+    def __init__(self, address, data, source, destination) -> None:
+        super().__init__(type="DataRespStore", source=source, destination=destination)
+        self.address = address
+        self.data = data      
+    def __str__(self) -> str:
+        return f"<{self.type}Message address={self.address}, data={self.data}, source={self.source}, destination={self.destination}>"
+
 
 class UnblockMessage(Message):
     def __init__(self, address, data, source, destination) -> None:
@@ -90,7 +130,7 @@ class Interconnect:
             return None 
         message = self.channel[0]
         
-        print(f"[Cycle {self.clock}]Processing {message}")
+        print(f"[Cycle {self.clock}][Interconnect] Processing {message}")
         del self.channel[0]
         if message.destination == 'Directory':
             self.directoryController.onMessage(message)
@@ -101,9 +141,10 @@ class Interconnect:
 
 
 class DirectoryController:
-    def __init__(self, clock) -> None:
+    def __init__(self, clock, memoryController: MemoryController) -> None:
         self.directoryState = {}
         self.clock = clock
+        self.memoryController = memoryController
 
     def attachInterconnect(self, interconnect: Interconnect):
         self.interconnect = interconnect
@@ -113,11 +154,28 @@ class DirectoryController:
             self.onGetS(message)
         elif isinstance(message, UnblockMessage):
             self.onUnblock(message)
+        elif isinstance(message, GetMMessage):
+            self.onGetM(message)
         else:
             print("Message type not identified!!!")
 
+    def onGetM(self, message:GetMMessage):
+        print(f"[Cycle {self.clock}] [Directory] {message}")
+        try:
+            directoryState = self.directoryState[message.address]
+            self.interconnect.sendMessage(DataRespAckMessage(message.address, directoryState.data, len(directoryState.sharers), "Directory", message.source))
+            for sharer in directoryState.sharers:
+                self.interconnect.sendMessage(InvalidateMessage(message.address, message.source, "Directory", sharer))
+            directoryState.state = 'B'
+
+        except KeyError:
+            data = self.memoryController.getData(message.address)
+            self.directoryState[message.address] = DirectoryStateEntry(message.address, data, 'M', True, set([message.source]))
+            self.interconnect.sendMessage(DataRespStoreMessage(message.address, data, "Directory", message.source))
+
+
     def onGetS(self, message:GetSMessage):
-        print(f"[Cycle {self.clock}] [Directory] {message.type}")
+        print(f"[Cycle {self.clock}] [Directory] {message}")
         try:
             directoryState = self.directoryState[message.address]
             if directoryState.state == 'M':
@@ -125,13 +183,16 @@ class DirectoryController:
                 # we forward the message there 
                 assert len(directoryState.sharers) == 1, "There cannot be more than 1 cores that hold the cache block in modified state."
 
-                self.interconnect.sendMessage(FwdGetSMessage(message.address, message.requester, "Directory", list(directoryState.sharers)[0]))
+                self.interconnect.sendMessage(FwdGetSMessage(message.address, message.source, "Directory", list(directoryState.sharers)[0]))
                 directoryState.state = 'B'
         except KeyError:
-            pass 
+            # Get the item from the main memory
+            data = self.memoryController.getData(message.address) # Memory operation has latency of 0 cycles!!!
+            self.directoryState[message.address] = DirectoryStateEntry(message.address, data, 'S', False, set([message.source]))
+            self.interconnect.sendMessage(DataRespLoadMessage(message.address, data, "Directory", message.source))
 
     def onUnblock(self, message: UnblockMessage):
-        print(f"[Cycle {self.clock}] [Directory] {message.type}")
+        print(f"[Cycle {self.clock}] [Directory] {message}")
         self.directoryState[message.address].data = message.data
         self.directoryState[message.address].dirty = True 
         self.directoryState[message.address].state = 'S'
@@ -149,9 +210,10 @@ class CacheController:
     def __init__(self, identifier, clock) -> None:
         self.cacheState = {}
         self.identifier = identifier
-        self.loadQueue = []
-        self.storeQueue = []
+        self.loadQueue = {}
+        self.storeQueue = {}
         self.clock = clock
+        self.ackQueue = {}
         self.state = 'READY'
 
     def attachInterconnect(self, interconnect: Interconnect):
@@ -162,8 +224,36 @@ class CacheController:
         if isinstance(message, FwdGetSMessage):
             self.onFwdGetS(message)
         elif isinstance(message, DataRespMessage):
-            self.onDataResp
+            self.onDataResp(message)
+        elif isinstance(message, InvalidateMessage):
+            self.onInvalidate(message)
+        elif isinstance(message, AckMessage):
+            self.onAck(message)
+        elif isinstance(message, DataRespLoadMessage):
+            self.onDataRespLoad(message)
+        elif isinstance(message, DataRespStoreMessage):
+            self.onDataRespLoad(message)
         
+        
+        
+    def onInvalidate(self, message:InvalidateMessage):
+        print(f"[Cycle {self.clock}] [CacheController {self.identifier} {self.state}] {message}")
+        assert message.address in self.cacheState.keys(), f"{message.address} must be present in the cache."
+        self.cacheState.pop(message.address)
+        self.interconnect.sendMessage(AckMessage(message.address, self.identifier, message.requester))
+
+    def onAck(self, message:AckMessage):
+        print(f"[Cycle {self.clock}] [CacheController {self.identifier} {self.state}] {message}")
+        try:
+            self.ackQueue[message.address] -= 1
+        except KeyError:
+            print(f"{message.address} must be present in the ack queue")
+            exit(-1)
+        if self.ackQueue[message.address] == 0:
+            self.ackQueue.pop(message.address)
+            self.cacheState[message.address].state = 'M'
+
+
     def loadData(self, address):
         if self.state == 'BUSY':
             print(f"[Cycle {self.clock}] [CacheController {self.identifier} {self.state}] loadData {address} cannot be processed")
@@ -179,11 +269,28 @@ class CacheController:
             self.state = 'BUSY'
 
     def onFwdGetS(self, message:FwdGetSMessage):
-        print(f"[Cycle {self.clock}] [CacheController {self.identifier} {self.state}] {message.type}")
+        print(f"[Cycle {self.clock}] [CacheController {self.identifier} {self.state}] {message}")
         self.interconnect.sendMessage(DataRespMessage(message.address, self.cacheState[message.address].data, self.identifier, message.requester))
 
+    def onDataRespLoad(self, message:DataRespLoadMessage):
+        print(f"[Cycle {self.clock}] [CacheController {self.identifier} {self.state}] {message}")
+        self.cacheState[message.address].address = message.address
+        self.cacheState[message.address].data = message.data
+        self.cacheState[message.address].state = 'S'
+        self.state = 'READY'
+        # self.interconnect.sendMessage(UnblockMessage(message.address, message.data, self.identifier, "Directory"))
+
+    def onDataRespStore(self, message:DataRespLoadMessage):
+        print(f"[Cycle {self.clock}] [CacheController {self.identifier} {self.state}] {message}")
+        self.cacheState[message.address].address = message.address
+        self.cacheState[message.address].data = message.data
+        self.cacheState[message.address].state = 'M'
+        self.state = 'READY'
+        # self.interconnect.sendMessage(UnblockMessage(message.address, message.data, self.identifier, "Directory"))
+
+
     def onDataResp(self, message:DataRespMessage):
-        print(f"[Cycle {self.clock}] [CacheController {self.identifier} {self.state}] {message.type}")
+        print(f"[Cycle {self.clock}] [CacheController {self.identifier} {self.state}] {message}")
         self.cacheState[message.address].address = message.address
         self.cacheState[message.address].data = message.data
         self.cacheState[message.address].state = 'S'
@@ -198,27 +305,35 @@ class CacheController:
             return None 
         print(f"[Cycle {self.clock}] [CacheController {self.identifier} {self.state}] storeData at {address}, data = {data}")
         try:
-            cacheBlockState = self.cacheState.state 
+            cacheBlockState = self.cacheState[address].state 
             if cacheBlockState == 'S':
                 pass 
             else:
                 pass 
         except KeyError:
-            self.interconnect.sendMessage(GetMMessage(address))
+            self.interconnect.sendMessage(GetMMessage(address, self.identifier, "Directory"))
             self.cacheState[address] = CacheStateEntry(address, 'X', 'IM')
+            self.storeQueue[address] = data
             self.state = 'BUSY'
 
 
 if __name__ == "__main__":
     clock = Clock()
-    directoryController = DirectoryController(clock)
+    memoryController = MemoryController(clock)
+    directoryController = DirectoryController(clock, memoryController)
     cacheController1 = CacheController("Core1", clock)
     cacheController2 = CacheController("Core2", clock)
+    cacheController3 = CacheController("Core3", clock)
     
     interconnect = Interconnect(directoryController, [cacheController1, cacheController2], clock)
     while True:
         if clock.counter == 0:
+            cacheController2.storeData(10, 500)
+        elif clock.counter == 5:
             cacheController1.loadData(10)
+        elif clock.counter == 10:
+            cacheController3.storeData(10, 200)
+
         interconnect.nextCycle()
         choice = input("Continue?(Y)es/(N)o")
         if choice == 'N':
